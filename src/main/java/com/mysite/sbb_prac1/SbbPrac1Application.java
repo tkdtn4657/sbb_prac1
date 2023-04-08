fdb72ed09bfdcd888d3b5aff790930c753907ed4
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-08 14:40:08 +0900

    init 2-01

---
+++ src/main/java/com/mysite/sbb_prac1/SbbPrac1Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbPrac1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbPrac1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/sbb_prac1/SbbPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbPrac1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
