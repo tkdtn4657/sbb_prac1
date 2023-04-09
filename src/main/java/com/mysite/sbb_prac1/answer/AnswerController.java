9a8be6389e7de91c75a3617836fa3b5e69628bf1
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-09 16:20:31 +0900

    3-12 앵커기능 추가 및 마크다운 스킵

--- src/main/java/com/mysite/sbb_prac1/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb_prac1/answer/AnswerController.java
@@ -34,9 +34,9 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-
-        answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -81,7 +81,8 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
 
