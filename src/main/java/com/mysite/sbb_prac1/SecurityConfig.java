4f4bc4add5e084fccac97e67be8a7525f22904f5
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-09 13:28:29 +0900

    3-07 로그인 로그아웃 기능 구현

--- src/main/java/com/mysite/sbb_prac1/SecurityConfig.java
+++ src/main/java/com/mysite/sbb_prac1/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.mysite.sbb_prac1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +18,17 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests().requestMatchers(
-                new AntPathRequestMatcher("/**")).permitAll();
+                new AntPathRequestMatcher("/**")).permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/")
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+        ;
         return http.build();
     }
 
@@ -24,5 +36,11 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 
 }
--- src/main/java/com/mysite/sbb_prac1/user/UserRepository.java
+++ src/main/java/com/mysite/sbb_prac1/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.mysite.sbb_prac1.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 
 }
